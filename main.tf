1127f6f463321226477d02383800d9a6a35b285e
nivedmct <nivedmct@outlook.com>
2024-10-11 14:54:24 +0530

    Add azure storage account

--- main.tf
+++ main.tf
@@ -15,3 +15,10 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
+resource "azurerm_storage_account" "stgacct" {
+  name = "stgacctnv7890"
+  resource_group_name = azurerm_resource_group.example.name
+  account_tier = "Standard"
+  location = azurerm_resource_group.example.location
+  account_replication_type = "LRS"
+}
